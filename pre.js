df8cb32436ca42a9917696d286adf6bb465f63c7
Jack Bonaguro <jackbonaguro@gmail.com>
2023-08-18 21:25:51 -0400

    typo

--- pre.js
+++ pre.js
@@ -1,7 +1,7 @@
 var Module = {
   'locateFile': function(path) {
     console.log('locateFile');
-    if (window.location.protocol === 'https') {
+    if (window.location.protocol.includes('https')) {
       const directory = window.location.pathname.split('/')[1];
       return '/' + directory + '/build/' + path;
     } else {
