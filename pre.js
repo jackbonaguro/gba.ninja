16e9b6a94a15f4b6353238454dbcca741174c0f2
Jack Bonaguro <jackbonaguro@gmail.com>
2023-08-18 22:08:37 -0400

    fix module

--- pre.js
+++ pre.js
@@ -1,11 +1,16 @@
-var Module = {
-  'locateFile': function(path) {
-    console.log('locateFile');
-    if (window.location.protocol.includes('https')) {
-      const directory = window.location.pathname.split('/')[1];
-      return '/' + directory + '/build/' + path;
-    } else {
-      return '/build/' + path;
-    }
+console.log('Module', Module);
+console.log('window.Module', window.Module);
+
+Module['locateFile'] = function(path) {
+  if (window.location.protocol.includes('https')) {
+    const directory = window.location.pathname.split('/')[1];
+    return '/' + directory + '/build/' + path;
+  } else {
+    return '/build/' + path;
   }
+};
+
+Module['onRuntimeInitialized'] = function() {
+    window.emuLoaded = true;
+    window.dispatchEvent(new Event('emuLoaded'));
 };
